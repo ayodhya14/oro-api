4042457651c2d179aea4951cd878c48af8305f6e
San <67090049+sandaniUma@users.noreply.github.com>
2020-08-23 15:24:43 +0530

    remove array from qty inside order model

--- models/order.js
+++ models/order.js
@@ -11,7 +11,7 @@ const orderSchema = new mongoose.Schema({
         required: true
     },
     qty: {
-        type: "Number",
+        type: Number,
         required: true
     },
     subTotal: {
