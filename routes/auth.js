66aacf68e3b2ab0789d0530d8dd0e9b06f022440
San <67090049+sandaniUma@users.noreply.github.com>
2020-09-08 21:59:46 +0530

    Update auth -Login with google and normal user login separate inside the post method

--- routes/auth.js
+++ routes/auth.js
@@ -12,38 +12,60 @@ const SECRET_KEY = "Y4w8atbcZFRfsWdMNvBea5TeqrUHtWLaVQURv25T1bU=";
 //User login
 router.post("/", async (req, res) => {
 
-     // Validations - All Feilds are empty
-     if(!req.body.mobile && !req.body.password){
-         return res.status(400).send("Not all mandotry values have been set!");
+
+     if(req.body.loginWithGoogle == false){
+
+          // Validations - All Feilds are empty
+          if(!req.body.mobile && !req.body.password){
+               return res.status(400).send("Not all mandotry values have been set!");
+          }
+     
+          //validation for Email
+          if(!req.body.email){
+               return res.status(400).send("Email cannot be blank!");
+          } 
+          else if (!req.body.email.includes("@" && ".com")){
+               return res.status(400).send("Invalid Email Address!");
+          } 
+     
+          //validation for Password
+          if(!req.body.password){
+               return res.status(400).send("Password cannot be blank!");
+          } 
      }
 
-     //validation for Email
-     if(!req.body.email){
-          return res.status(400).send("Email cannot be blank!");
-     } 
-     else if (!req.body.email.includes("@" && ".com")){
-          return res.status(400).send("Invalid Email Address!");
-     } 
-
-      //validation for Password
-     if(!req.body.password){
-          return res.status(400).send("Password cannot be blank!");
-     } 
-
      try{
-        let user = await User.findOne({ email: req.body.email});
-        if(!user) return res.status(400).send("Invalid Email or Password!");
+        
+          if(req.body.loginWithGoogle == false){
 
-        //compare the encrypted password
-        let validpassword =await bcrypt.compare(req.body.password, user.password);
-        if (!validpassword) return res.status(400).send("Invalid Email or Password!");  
-
-        //create jwt token using user id and email
-        let token = jwt.sign({id: user._id, email: user.email}, SECRET_KEY, {
-          //    expiresIn: "24h"
-        });
+               //----NORMAL USER LOGIN------
+               //compare email
+               let user = await User.findOne({ email: req.body.email});
+               if(!user) return res.status(400).send("Invalid Email or Password!");
        
-        res.send({
+               //compare the encrypted password
+               let validpassword =await bcrypt.compare(req.body.password, user.password);
+               if (!validpassword) return res.status(400).send("Invalid Email or Password!");  
+       
+               //create jwt token using user id and email
+               let token = jwt.sign({id: user._id, email: user.email}, SECRET_KEY, {
+                 //    expiresIn: "24h"
+               });
+          }
+          else{
+
+               //----SIGN IN WITH GOOGLE LOGIN------
+               //compare email
+               let user = await User.findOne({ email: req.body.email});
+               if(!user) return res.status(400).send("Invalid Email or Password!");
+       
+               //create jwt token using user id and email
+               let token = jwt.sign({id: user._id, email: user.email}, SECRET_KEY, {
+                 //    expiresIn: "24h"
+               });
+          }
+       
+          res.send({
               token: token,
               id: user._id,
               firstName: user.firstName,
